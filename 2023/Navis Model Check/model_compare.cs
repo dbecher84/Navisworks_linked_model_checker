39d7a290ee3c8a6c7480b8f89cd8e8ee1cee180c
dbecher84 <dbecher84@gmail.com>
2022-12-21 13:38:12 -0500

    annotated

--- 2023/Navis Model Check/model_compare.cs
+++ 2023/Navis Model Check/model_compare.cs
@@ -14,20 +14,14 @@ namespace Navis_Model_Check
 {
     public partial class model_compare : Form
     {
+        ////initials form to display models
+        ////inputs model list from csv as string and model list from navis as string
         public model_compare(List<string> FromFile, List<string> FoundList)
         {
             InitializeComponent();
 
-            //string[] FromFile = File.ReadAllLines(Models.path);
-
-            //List<string> cleanDisplay = new List<string>();
-
             foreach (string item in FromFile)
             {
-            //    string fmodel = item.Substring(0, item.LastIndexOf(','));
-            //    string fdate = item.Substring(item.LastIndexOf(',') + 1);
-            //    //MessageBox.Show(fmodel + "----" + fdate);
-            //    //cleanDisplay.Add(fmodel);
                 listBox1.Items.Add(item);
             }
             foreach (string item2 in FoundList)
@@ -35,6 +29,8 @@ namespace Navis_Model_Check
                 listBox2.Items.Add(item2);
             }
 
+            ////checks if file found in navis is in saved csv list
+            ////if not model is added to box 3 (list of models added since check)
             foreach (string item3 in FoundList)
             {
                 if (FromFile.Contains(item3) == false)
@@ -46,6 +42,9 @@ namespace Navis_Model_Check
 
                 }
             }
+
+            ////checks if file found in saved csv list is in navis
+            ////if not model is added to box 4 (list of models removed since check)
             foreach (string item4 in FromFile)
             {
                 if (FoundList.Contains(item4) == false)
@@ -84,18 +83,24 @@ namespace Navis_Model_Check
 
         }
 
+        //// button will replace an existing csv file wit a new one containing the updated model list
         private void button1_Click(object sender, EventArgs e)
         {
+            ////searches model file path for project number for use in csv file name
             var regMatchNum = @"([0-9]{5})";
             Match regMatchnum = Regex.Match(Models.nFilePath, regMatchNum);
             string projectNum = regMatchnum.Value;
 
+            ////path to save csv file
             string buttonPath = Models.path1 + @"\" + projectNum + Models.path2;
 
+            ////delete existing csv file
             File.Delete(buttonPath);
 
+            ////create new updated csv file
             List<string> newList = getmodels.getnavismodels(Models.document);
             writecsv.writeToCsv(newList, buttonPath);
+
             MessageBox.Show("The list of models has been updated", "File Updated");
         }
     }
